3ca561397d26c9862f5210c966500ef2c061894a
samina <saminaquayum1@gmail.com>
2022-08-22 22:41:41 -0700

    gitgit

---
+++ src/main/java/com/gitprac/gitprac/App.java
@@ -0,0 +1,13 @@
+package com.gitprac.gitprac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/gitgit/samsam.java
@@ -0,0 +1,10 @@
+package gitgit;
+
+public class samsam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
